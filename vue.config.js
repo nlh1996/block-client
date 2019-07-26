59882b000cb40ee69cadf2be855fac3ea3db1bb1
nlh1996 <1217286494@qq.com>
2019-07-26 14:21:18 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+// vue.config.js
+
+module.exports = {
+  // 基本路径
+  publicPath: './',
+  // 输出文件目录
+  outputDir: 'dist',
+  // webpack-dev-server 相关配置
+  // devServer: {
+  //   port: 8080,
+  // },
+}
